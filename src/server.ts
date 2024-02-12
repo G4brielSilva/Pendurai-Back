b511791314a3d269ccf45be91ee1df949af06f9b
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2024-02-12 19:54:57 -0300

    refactor: reorganizando inicialização do server e adicionando swagger na api

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,29 @@
+/* eslint-disable no-console */
+import express from 'express';
 import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import { dataSource } from './config/database';
 import { swaggerConfig } from './config/swagger';
+import { routes } from './routes';
 
-dataSource
-    .initialize()
-    .then(async () => {
-        console.log('Database has been initialized');
+const app = express();
+
+app.use(express.json());
+app.use('/', routes);
+
+async function startServer(): Promise<void> {
+    try {
+        await dataSource.initialize();
         await dataSource.runMigrations();
-        console.log('Migration has been run');
-    })
-    .catch(error => console.error('Error initializing database:', error));
 
-swaggerJSDoc(swaggerConfig);
+        app.listen(3000, () => console.log('Server is running on port 3000'));
+    } catch (error) {
+        console.error('Error initializing server:', error);
+    }
+}
+
+const swaggerSpecs = swaggerJSDoc(swaggerConfig);
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+
+startServer();
+export { app };
