2ee5149b3066955ae6f7db5d0eab764b68398a54
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2024-02-13 21:54:53 -0300

    chore: alterando configurações do projeto

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
     clearMocks: true,
     collectCoverageFrom: ['src/endpoints/**/*.ts', '!src/**/*.entity.ts', '!src/**/*.repository.ts', '!src/**/index.ts'],
     preset: 'ts-jest',
-    testEnvironment: 'node'
+    testEnvironment: 'node',
+    setupFiles: ['<rootDir>/jest/setEnvVars.ts']
 };
