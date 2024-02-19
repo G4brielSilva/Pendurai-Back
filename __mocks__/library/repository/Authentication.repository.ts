581d870a0600b242101f2c503ccbd0c145acafc8
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2024-02-18 22:02:27 -0300

    test: criando endpoint de cadastro de usuário

--- __mocks__/library/repository/Authentication.repository.ts
+++ __mocks__/library/repository/Authentication.repository.ts
@@ -22,10 +22,23 @@ export class AuthenticationRepository {
                 id: 'deleted_user_id',
                 deletedAt: new Date().toISOString()
             }
+        },
+        {
+            id: '62d5bb3b188314032c4f7815',
+            email: 'registered_account_email@email.com',
+            password: 'valid_hashed_password',
+            salt: '123',
+            user: {
+                id: 'deleted_user_id'
+            }
         }
     ] as unknown as Authentication[];
 
-    public getHashedPassword = jest.fn().mockImplementation((email: string): Promise<Authentication> => {
+    public findByEmail = jest.fn().mockImplementation((email: string): Promise<Authentication> => {
         return Promise.resolve(this.mockList.find(auth => auth.email === email) as Authentication);
+    });
+
+    public insert = jest.fn().mockImplementation((): Promise<Authentication> => {
+        return Promise.resolve(this.mockList[0]);
     });
 }
--- __tests__/endpoints/AuthenticationController.test.ts
+++ __tests__/endpoints/AuthenticationController.test.ts
@@ -8,6 +8,11 @@ jest.mock('../../src/library/repository/Authentication.repository', () => {
     return jest.requireActual('../../__mocks__/library/repository/Authentication.repository');
 });
 
+// User repository mock
+jest.mock('../../src/library/repository/User.repository', () => {
+    return jest.requireActual('../../__mocks__/library/repository/User.repository');
+});
+
 // JWT mock
 jest.mock('../../src/third-party/Jwt', () => {
     return jest.requireActual('../../__mocks__/third-party/Jwt');
@@ -25,7 +30,7 @@ describe('AuthenticationController', () => {
 
         const validLoginCredentials = {
             email: 'valid_email@email.com',
-            password: 'valid_password'
+            password: 'V@lid_P4ssw0rd'
         }
 
         const spyVerifyPassword = jest.spyOn(Password, 'verifyPassword');
@@ -33,7 +38,7 @@ describe('AuthenticationController', () => {
         it('should return 400 if an invalid email was provided', async () => {
             const response = await request(app).post(URL).send({
                 ...validLoginCredentials,
-                email: 'invalid_email@email.com'
+                email: 'invalid_email'
             });
 
             expect(response.status).toBe(400);
@@ -42,7 +47,7 @@ describe('AuthenticationController', () => {
         it('should return 400 if an invalid password was provided', async () => {
             const response = await request(app).post(URL).send({
                 ...validLoginCredentials,
-                password: 'invalid_password'
+                password: '123'
             });
 
             expect(response.status).toBe(400);
@@ -52,11 +57,9 @@ describe('AuthenticationController', () => {
             spyVerifyPassword.mockReturnValueOnce(true);
 
             const response = await request(app).post(URL).send({
-                email: 'deleted_user_email@email.com',
-                password: 'an_valid_password'
+                ...validLoginCredentials,
+                email: 'deleted_user_email@email.com'
             });
-
-            console.log(response.body);
 
             expect(response.status).toBe(400);
         });
@@ -65,6 +68,69 @@ describe('AuthenticationController', () => {
             spyVerifyPassword.mockReturnValueOnce(true);
 
             const response = await request(app).post(URL).send(validLoginCredentials);
+
+            expect(response.status).toBe(200);
+        });
+    });
+
+    describe('POST - register', () => {
+        const URL = '/api/auth/register';
+
+        const validRegisterCredentials = {
+            email: 'valid_email_to_register@email.com',
+            password: 'V@lid_P4ssw0rd',
+            passwordConfirmation: 'V@lid_P4ssw0rd',
+            name: 'valid_name'
+        }
+
+        it('should return 400 if an invalid email was provided', async () => {
+            const response = await request(app).post(URL).send({
+                ...validRegisterCredentials,
+                email: 'invalid_email'
+            });
+
+            expect(response.status).toBe(400);
+        });
+
+        it('should return 400 if an invalid password was provided', async () => {
+            const response = await request(app).post(URL).send({
+                ...validRegisterCredentials,
+                password: '123'
+            });
+
+            expect(response.status).toBe(400);
+        });
+
+        it('should return 400 if passwordConfirmation is different to password', async () => {
+            const response = await request(app).post(URL).send({
+                ...validRegisterCredentials,
+                passwordConfirmation: 'an_password_confirmation'
+            });
+
+            expect(response.status).toBe(400);
+        });
+
+        it('should return 400 if an invalid name is provided', async () => {
+            const response = await request(app).post(URL).send({
+                ...validRegisterCredentials,
+                name: 'ab'
+            });
+
+            expect(response.status).toBe(400);
+        });
+
+        it('should return 400 if a register is tried with an existing account', async () => {
+            const response = await request(app).post(URL).send({
+                ...validRegisterCredentials,
+                email: 'registered_account_email@email.com'
+            });
+
+            expect(response.status).toBe(400);
+        });
+
+        it('should return 200 if valid email and password was provided', async () => {
+            const response = await request(app).post(URL).send(validRegisterCredentials);
+
             expect(response.status).toBe(200);
         });
     });
