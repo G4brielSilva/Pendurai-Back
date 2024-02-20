0d60c15f5dae4c8eefe242fc4f5356fb58d2a6fe
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2024-02-20 16:36:16 -0300

    feat: adicionando Redis ao projeto para armazenar os tokens invalidados do logout numa blacklist

--- src/third-party/Jwt.ts
+++ src/third-party/Jwt.ts
@@ -1,32 +1,25 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-console */
-import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
-import { RouteResponse } from '../common/models/RouteResponse';
+import { TObject } from '../common/models/TObject';
+import { Redis } from './Redis';
 
 export class JWT {
     // eslint-disable-next-line consistent-return
-    public static authenticateToken(req: Request, res: Response, next: NextFunction): void {
-        const authHeader = req.headers.authorization;
-        const token = authHeader && authHeader.split(' ')[1];
+    public static async deactiveToken(authorizationToken: string | undefined): Promise<string | void> {
+        if (!authorizationToken) return Promise.resolve();
 
-        if (token == null) return RouteResponse.unauthorized('Unauthorized', res);
-
-        jwt.verify(token, process.env.JWT_TOKEN as string);
-
-        next();
+        const token = authorizationToken.split(' ')[1];
+        return new Redis().addTokenBlackList(token);
     }
 
     public static generateAccessToken(userId: string, authId: string, role: string): string {
         return jwt.sign({ userId, authId, role }, process.env.JWT_TOKEN as string, { expiresIn: '8h' });
     }
 
-    public static decodeToken(token: string): any {
-        try {
-            return jwt.verify(token, process.env.JWT_TOKEN as string);
-        } catch (error: Error | any) {
-            console.log('Error decoding token:', error.message);
-            return null;
-        }
+    public static async decodeToken(token: string): Promise<TObject> {
+        if (await new Redis().connectAndgetTokenByBlackList(token)) return { error: 'Token is deactivated' };
+        const result = jwt.verify(token, process.env.JWT_TOKEN as string);
+        return Promise.resolve(result as TObject);
     }
 }
