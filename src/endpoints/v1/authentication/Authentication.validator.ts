51b45d99f831e6e029b0801d84b8a9fbd04825b4
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2024-02-26 18:32:09 -0300

    refactor: concatenando ParamsSchema num objeto de model do validator

--- src/endpoints/v1/authentication/Authentication.validator.ts
+++ src/endpoints/v1/authentication/Authentication.validator.ts
@@ -1,56 +1,74 @@
 import { RequestHandler } from 'express';
+import { Schema } from 'express-validator';
 import { BaseValidator } from '../../../common/models/BaseValidator';
 import { AuthenticationRepository } from '../../../library/repository';
 import { Password } from '../../../utils/Password';
 
+// TODO: adicionar model para poupar repetição de validação
 export class AuthenticationValidator extends BaseValidator {
+    private static model: Schema = {
+        email: {
+            in: 'body',
+            isEmail: true,
+            custom: {
+                options: async (email: string, { req }): Promise<void> => {
+                    const authentication = await new AuthenticationRepository().findByEmail(email);
+
+                    if (!authentication || authentication.user.deletedAt) return Promise.reject();
+
+                    req.body.authentication = authentication;
+                    return Promise.resolve();
+                },
+                errorMessage: 'Email is not registered'
+            },
+            errorMessage: 'Credentials are invalid'
+        },
+        password: {
+            in: 'body',
+            isString: true,
+            isLength: {
+                options: { min: 8, max: 32 },
+                errorMessage: 'Credentials are invalid'
+            },
+            matches: {
+                options: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_])[A-Za-z\d@$!%*?&_]*$/], // Senha com pelo menos 8 caracteres; um caracter especial; um caracter maiusculo; um caracter minusculo; um número
+                errorMessage: 'The password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.'
+            },
+            custom: {
+                options: async (password: string, { req }): Promise<void> => {
+                    const { authentication } = req.body;
+
+                    const { password: hashedPassword, salt } = authentication;
+                    if (!authentication || !Password.verifyPassword(password, hashedPassword, salt as string)) return Promise.reject();
+
+                    return Promise.resolve();
+                }
+            },
+            errorMessage: 'Credentials are invalid'
+        },
+        recoveryCode: {
+            in: 'body',
+            isString: true,
+            isLength: { options: { min: 6, max: 6 } },
+            custom: {
+                options: async (recoveryCode: string, { req }): Promise<void> => {
+                    const isValid = await Password.recoveryCodeIsValid(req.body.email, recoveryCode);
+
+                    return isValid ? Promise.resolve() : Promise.reject();
+                }
+            },
+            errorMessage: 'invalid recoveryCode'
+        }
+    };
+
     /**
      * login
      * @return { Array<RequestHandler> }
      */
     public static login(): Array<RequestHandler> {
-        return AuthenticationValidator.validationList({
-            email: {
-                in: 'body',
-                isEmail: true,
-                errorMessage: 'Credentials are invalid',
-                custom: {
-                    options: async (email: string, { req }): Promise<void> => {
-                        const authentication = await new AuthenticationRepository().findByEmail(email);
+        const { email, password } = AuthenticationValidator.model;
 
-                        if (!authentication || authentication.user.deletedAt) return Promise.reject();
-
-                        req.body.authentication = authentication;
-                        return Promise.resolve();
-                    },
-                    errorMessage: 'Email is not registered'
-                }
-            },
-            password: {
-                in: 'body',
-                isString: true,
-                isLength: {
-                    options: { min: 8, max: 32 },
-                    errorMessage: 'Credentials are invalid'
-                },
-                matches: {
-                    options: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_])[A-Za-z\d@$!%*?&_]*$/], // Senha com pelo menos 8 caracteres; um caracter especial; um caracter maiusculo; um caracter minusculo; um número
-                    errorMessage:
-                        'The password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.'
-                },
-                custom: {
-                    options: async (password: string, { req }): Promise<void> => {
-                        const { authentication } = req.body;
-
-                        const { password: hashedPassword, salt } = authentication;
-                        if (!authentication || !Password.verifyPassword(password, hashedPassword, salt as string)) return Promise.reject();
-
-                        return Promise.resolve();
-                    }
-                },
-                errorMessage: 'Credentials are invalid'
-            }
-        });
+        return AuthenticationValidator.validationList({ email, password });
     }
 
     /**
@@ -58,22 +76,9 @@ export class AuthenticationValidator extends BaseValidator {
      * @return { Array<RequestHandler> }
      */
     public static forgotPassword(): Array<RequestHandler> {
-        return AuthenticationValidator.validationList({
-            email: {
-                in: 'body',
-                isEmail: true,
-                custom: {
-                    options: async (email: string): Promise<void> => {
-                        const authentication = await new AuthenticationRepository().findByEmail(email);
+        const { email } = AuthenticationValidator.model;
 
-                        if (!authentication || authentication.user.deletedAt) return Promise.reject();
-                        return Promise.resolve();
-                    },
-                    errorMessage: 'Email is not registered'
-                },
-                errorMessage: 'Credentials are invalid'
-            }
-        });
+        return AuthenticationValidator.validationList({ email });
     }
 
     /**
@@ -81,19 +86,13 @@ export class AuthenticationValidator extends BaseValidator {
      * @return { Array<RequestHandler> }
      */
     public static verifyRecoveryCode(): Array<RequestHandler> {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { in: inValue, ...recoveryCodeParams } = AuthenticationValidator.model.recoveryCode;
+
         return AuthenticationValidator.validationList({
             recoveryCode: {
-                in: 'params',
-                isString: true,
-                isLength: { options: { min: 6, max: 6 } },
-                custom: {
-                    options: async (recoveryCode: string, { req }): Promise<void> => {
-                        const isValid = await Password.recoveryCodeIsValid(req.body.email, recoveryCode);
-
-                        return isValid ? Promise.resolve() : Promise.reject();
-                    }
-                },
-                errorMessage: 'invalid recoveryCode'
+                ...recoveryCodeParams,
+                in: 'params'
             }
         });
     }
@@ -103,36 +102,11 @@ export class AuthenticationValidator extends BaseValidator {
      * @return { Array<RequestHandler> }
      */
     public static changePassword(): Array<RequestHandler> {
+        const { email, recoveryCode } = AuthenticationValidator.model;
+
         return AuthenticationValidator.validationList({
-            email: {
-                in: 'body',
-                isEmail: true,
-                custom: {
-                    options: async (email: string, { req }): Promise<void> => {
-                        const authentication = await new AuthenticationRepository().findByEmail(email);
-
-                        if (!authentication || authentication.user.deletedAt) return Promise.reject();
-
-                        req.body.authentication = authentication;
-                        return Promise.resolve();
-                    },
-                    errorMessage: 'Email is not registered'
-                },
-                errorMessage: 'Credentials are invalid'
-            },
-            recoveryCode: {
-                in: 'body',
-                isString: true,
-                isLength: { options: { min: 6, max: 6 } },
-                custom: {
-                    options: async (recoveryCode: string, { req }): Promise<void> => {
-                        const isValid = await Password.recoveryCodeIsValid(req.body.email, recoveryCode);
-
-                        return isValid ? Promise.resolve() : Promise.reject();
-                    }
-                },
-                errorMessage: 'invalid recoveryCode'
-            },
+            email,
+            recoveryCode,
             newPassword: {
                 in: 'body',
                 isString: true,
