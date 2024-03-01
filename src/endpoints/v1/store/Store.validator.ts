8e2c6ba502337d06a5954f4160b320f12878acc9
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2024-03-01 19:38:44 -0300

    feat: criando endpoint de edição de lojas

--- src/endpoints/v1/store/Store.validator.ts
+++ src/endpoints/v1/store/Store.validator.ts
@@ -1,14 +1,16 @@
-import { RequestHandler } from 'express';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { BaseValidator } from '../../../common/models/BaseValidator';
+import { RouteResponse } from '../../../common/models/RouteResponse';
 import { Validations } from '../../../common/models/Validations';
+import { EnumRoles } from '../../../common/models/enum/EnumRoles';
 import { StoreRepository } from '../../../library/repository';
 
 export class StoreValidator extends BaseValidator {
     /**
-     * createStore
+     * storeData - Valida dados da loja (name & cnpj)
      * @return { Array<RequestHandler> }
      */
-    public static createStore(): Array<RequestHandler> {
+    public static storeData(): Array<RequestHandler> {
         const { cnpj } = Validations;
         return StoreValidator.validationList({
             name: {
@@ -24,21 +26,41 @@ export class StoreValidator extends BaseValidator {
         });
     }
 
-    public static listStoreById(): Array<RequestHandler> {
-        return StoreValidator.validationList({
-            storeId: {
-                in: 'params',
-                isString: true,
-                custom: {
-                    options: async (storeId: string, { req }): Promise<void> => {
-                        const store = await new StoreRepository().findById(storeId);
-                        req.body.storeId = storeId;
+    // /**
+    //  * createStore
+    //  * @return { Array<RequestHandler> }
+    //  */
+    // public static updateStore(): Array<RequestHandler> {
+    //     const { cnpj } = Validations;
+    //     return StoreValidator.validationList({
+    //         name: {
+    //             in: 'body',
+    //             isString: true,
+    //             isLength: {
+    //                 options: { min: 3, max: 254 },
+    //                 errorMessage: 'name must be at least 3 characters long'
+    //             },
+    //             errorMessage: 'name is invalid'
+    //         },
+    //         cnpj
+    //     });
+    // }
 
-                        return store ? Promise.resolve() : Promise.reject();
-                    }
-                },
-                errorMessage: 'storeId is invalid'
-            }
-        });
+    /**
+     * onlyId - Verifica se o id passado no path é válido e corresponde ao usuário autenticado
+     */
+    public static async onlyId(req: Request, res: Response, next: NextFunction): Promise<void> {
+        const { storeId } = req.params;
+
+        const store = await new StoreRepository().findById(storeId);
+
+        if (!store) return RouteResponse.badRequest(res, 'invalid StoreId');
+
+        const { role, userId } = req.body.authentication;
+
+        if (store?.owner !== userId || role !== EnumRoles.ADMIN) return RouteResponse.unauthorized(res);
+
+        req.body.storeId = storeId;
+        return next();
     }
 }
