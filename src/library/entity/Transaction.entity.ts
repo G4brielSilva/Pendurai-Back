402f6d1e0be35352e8045b17d5bc38b23a351ad5
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2024-04-03 15:37:02 -0300

    feat: adicionando Entidade de Transação

---
+++ src/library/entity/Transaction.entity.ts
@@ -0,0 +1,28 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { EnumTransactions } from '../../common/models/enum/EnumTransactions';
+import { ShopCart } from './ShopCart.entity';
+
+@Entity('transactions')
+export class Transaction {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date;
+
+    @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+    updatedAt: Date;
+
+    @Column({ type: 'timestamp', nullable: true })
+    deletedAt: Date;
+
+    @Column({ type: 'timestamp', nullable: true })
+    payedAt: Date;
+
+    @OneToOne(() => ShopCart)
+    @JoinColumn()
+    cart: ShopCart;
+
+    @Column({ type: 'enum', enum: EnumTransactions })
+    transactionType: EnumTransactions;
+}
