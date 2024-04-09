fc87a4e02d33bf51f51ebf633f0188da18cd91b9
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2024-04-09 16:51:58 -0300

    feat: adicionando funcionalidade de fechar carrinho e alteração de estoque

--- src/library/repository/Cart.repository.ts
+++ src/library/repository/Cart.repository.ts
@@ -1,6 +1,7 @@
 import { IsNull } from 'typeorm';
 import { BaseRepository } from '../../common/models/BaseRepository';
 import { ShopCart } from '../entity/ShopCart.entity';
+import { StockRepository } from './Stock.repository';
 
 export class CartRepository extends BaseRepository(ShopCart) {
     /**
@@ -10,5 +11,22 @@ export class CartRepository extends BaseRepository(ShopCart) {
      */
     public async findByStoreId(storeId: string): Promise<ShopCart | null> {
         return this.repository.findOne({ where: { store: { id: storeId }, closedAt: IsNull() } });
+    }
+
+    /**
+     * closeCart
+     * @param { string } cartId
+     * @returns { Promise<ShopCart> }
+     */
+    public async closeCart(cartId: string): Promise<ShopCart> {
+        const cart = await this.findById(cartId);
+
+        await Promise.all(
+            (cart?.cartItems ?? []).map(async item => {
+                await new StockRepository().transactionStockChange(item.storeItem.id, item.quantity);
+            })
+        );
+
+        return this.update(cartId, { closedAt: new Date() });
     }
 }
